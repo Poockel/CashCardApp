b6e8799c5c93bcf8636dd6f526f726c172e6585e
Wilson Lim <115963037+Poockel@users.noreply.github.com>
2022-11-22 12:32:51 +0800

    Add files via upload

---
+++ s10242034_CashCardApp/s10242034_CashCardApp/CashCard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CashCard
+{
+    private string id;
+    private double balance;
+        
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public double Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public CashCard() { }
+
+    public CashCard(string i, double b)
+    {
+        Id = i;
+        Balance = b;
+    }
+
+    public void TopUp(double a)
+    {
+        Balance = Balance + a;
+    }
+
+    public bool Deduct(double a)
+    {
+        if (a <= Balance)
+        {
+            Balance = Balance - a;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Id" + Id + "Balance" + Balance;
+    }
+
+    
+}
+
---
+++ s10242034_CashCardApp/s10242034_CashCardApp/Program.cs
@@ -0,0 +1,58 @@
+﻿void initcardlist(List<CashCard> cardlist)
+{
+    CashCard c1 = new CashCard("001", 25.0);
+    CashCard c2 = new CashCard("002", 25.0);
+    CashCard c3 = new CashCard("003", 30.0);
+    CashCard c4 = new CashCard("004", 30.0);
+    CashCard c5 = new CashCard("005", 50.0);
+    cardlist.Add(c1);
+    cardlist.Add(c2);
+    cardlist.Add(c3);
+    cardlist.Add(c4);
+    cardlist.Add(c5);
+}
+
+List<CashCard> cardlist = new();
+initcardlist(cardlist);
+
+CashCard Search(List<CashCard> cardlist, string id)
+{
+    foreach (var card in cardlist)
+    {
+        if (card.Id == id)
+        {
+            return card;
+        }
+    }
+    return null;
+}
+
+foreach (var c in cardlist)
+{
+    Console.WriteLine("{0}  {1}", c.Id, c.Balance);
+}
+
+Console.Write("Enter Id: ");
+String id = Console.ReadLine();
+CashCard card = Search(cardlist, id);
+
+if (card != null)
+{
+    Console.WriteLine("Balance" + card.Balance);
+    Console.WriteLine("Enter amount to deduct: ");
+    double amount = Convert.ToDouble(Console.ReadLine());
+    bool success = card.Deduct(amount);
+    if(success)
+    {
+        Console.WriteLine("Successful deduction");
+        Console.WriteLine("Balance" + card.Balance);
+    }
+    else
+    {
+        Console.WriteLine("Insufficient balance");
+    }
+}
+else
+{
+    Console.WriteLine("Card not found");
+}
